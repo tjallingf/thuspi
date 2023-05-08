bee0d5a1de98b5a2386a748359fa49ee2c5d5f74
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 21:07:25 +0200

    feat: accept AxiosRequestConfig argument

--- client/src/hooks/useQuery.ts
+++ client/src/hooks/useQuery.ts
@@ -1,37 +1,39 @@
 import { useQuery as originalUseQuery, UseQueryResult as OriginalUseQueryResult } from '@tanstack/react-query';
 import fetchQuery from '@/utils/fetchQuery';
+import { AxiosRequestConfig } from 'axios';
 
 export type ApiResponseData<TResult> = {
-  result: TResult;
-  aggregation: any;
+    result: TResult;
+    aggregation: any;
 };
 
 export type UseQueryOptions<TResult, TAggregation> = Partial<{
-  onSuccess: (result: TResult, aggregation: TAggregation) => any;
+    onSuccess: (result: TResult, aggregation: TAggregation) => any;
 }>;
 
 export type UseQueryResult<TResult, TAggregation> = OriginalUseQueryResult & {
-  result: TResult;
-  aggregation: TAggregation;
+    result: TResult;
+    aggregation: TAggregation;
 };
 
 export default function useQuery<TResult = unknown, TAggregation = any>(
-  endpoint: string,
-  options: UseQueryOptions<TResult, TAggregation> = {},
+    endpoint: string,
+    options: UseQueryOptions<TResult, TAggregation> = {},
+    config: AxiosRequestConfig = {},
 ): UseQueryResult<TResult, TAggregation> {
-  const query = originalUseQuery<ApiResponseData<TResult>>({
-    onSuccess: (data: ApiResponseData<TResult>) => {
-      return options.onSuccess?.(data.result, data.aggregation);
-    },
-    queryKey: [endpoint],
-    queryFn: async (): Promise<any> => {
-      return fetchQuery(endpoint);
-    },
-  } as any);
+    const query = originalUseQuery<ApiResponseData<TResult>>({
+        onSuccess: (data: ApiResponseData<TResult>) => {
+            return options.onSuccess?.(data.result, data.aggregation);
+        },
+        queryKey: [endpoint],
+        queryFn: async (): Promise<any> => {
+            return fetchQuery(endpoint, config);
+        },
+    } as any);
 
-  return {
-    ...query,
-    aggregation: query?.data?.aggregation,
-    result: query?.data?.result,
-  };
+    return {
+        ...query,
+        aggregation: query?.data?.aggregation,
+        result: query?.data?.result,
+    };
 }
