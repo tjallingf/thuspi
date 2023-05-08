838cb9d97dfff3c9c30a098c9dc4ffdfa4e6f515
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 22:00:12 +0200

    BREAKING CHANGE: rename FlowBlocklyEditor to FlowEditor

---
+++ client/src/Flows/components/FlowEditor/FlowEditor.tsx
@@ -0,0 +1,144 @@
+import React, { memo, useEffect, useState, useRef } from 'react';
+import * as Blockly from 'blockly/core';
+import '@blockly/field-date';
+import '@blockly/block-plus-minus';
+import BlocklyWorkspace from '@/Blockly/BlocklyWorkspace';
+import FlowEditorBlock from '@/utils/flows/FlowEditorBlock';
+import useQuery from '@/hooks/useQuery';
+import { useIntl } from 'react-intl';
+import fieldBlocks from '../../../utils/flows/blockly/fieldBlocks';
+import '../../assets/blockly/custom-fields/index';
+import useSocket from '@/hooks/useSocket';
+import { FlowBlockCategoryManifest } from '@server/flows/FlowBlockCategory';
+
+export interface IFlowEditorProps {
+    flowId: string;
+    blocks: Array<{
+        type: string;
+        manifest: any;
+    }>;
+    blockCategories: Array<{
+        extensionId: string;
+        id: string;
+        manifest: FlowBlockCategoryManifest;
+    }>;
+}
+const FlowEditor: React.FunctionComponent<IFlowEditorProps> = memo(({ flowId, blocks, blockCategories }) => {
+    const { formatMessage } = useIntl();
+    const blocklyWorkspaceRef = useRef<Blockly.WorkspaceSvg>(null);
+    const blocklySerializerRef = useRef(new Blockly.serialization.blocks.BlockSerializer());
+    const [toolbox, setToolbox] = useState(null);
+    const flowEditorBlocks = useRef<FlowEditorBlock[]>([]);
+
+    const LOCAL_STORAGE_AUTOSAVE_KEY = `flows-${flowId}-autosaved`;
+
+    function handleInject(workspace: Blockly.WorkspaceSvg) {
+        blocklyWorkspaceRef.current = workspace;
+
+        // Add change listener for autosaving
+        blocklyWorkspaceRef.current.addChangeListener(handleChange);
+
+        // Load the autosaved workspace
+        loadAutoSavedWorkspace();
+    }
+
+    function handleChange(e) {
+        if (['move', 'delete', 'change'].includes(e.type)) {
+            autoSaveWorkspace();
+        }
+    }
+
+    function autoSaveWorkspace() {
+        console.log('autosave', blocklyWorkspaceRef.current);
+        if (!blocklyWorkspaceRef.current) return;
+
+        // Serialize the workspace
+        const serializedState = blocklySerializerRef.current.save(blocklyWorkspaceRef.current);
+
+        // Remove from the user's localStorage if the workspace is empty
+        if (!serializedState) {
+            localStorage.removeItem(LOCAL_STORAGE_AUTOSAVE_KEY);
+            return;
+        }
+
+        // Store the serialized workspace in the user's localStorage
+        localStorage.setItem(LOCAL_STORAGE_AUTOSAVE_KEY, JSON.stringify(serializedState));
+    }
+
+    function loadAutoSavedWorkspace() {
+        try {
+            const serializedState = localStorage.getItem(LOCAL_STORAGE_AUTOSAVE_KEY);
+            if (serializedState.length) {
+                blocklySerializerRef.current.load(JSON.parse(serializedState), blocklyWorkspaceRef.current);
+            }
+        } catch (err) {}
+    }
+
+    function registerBlocks() {
+        // TODO: remove field blocks (see @zylax/core.logic_boolean)
+        Blockly.defineBlocksWithJsonArray(fieldBlocks);
+
+        blocks.forEach((block) => {
+            if (!block.manifest) {
+                console.error(`Invalid manifest for block '${block.type}': ${block.manifest}.`);
+                return true;
+            }
+
+            const flowEditorBlock = new FlowEditorBlock(block.type, block.manifest, {
+                messageFormatter: formatMessage,
+                category: blockCategories.find((c) => c.id === block.manifest.category)?.manifest,
+            });
+
+            Blockly.Blocks[block.type] = flowEditorBlock.getBlocklyBlockDef();
+            flowEditorBlocks.current.push(flowEditorBlock);
+        });
+    }
+
+    function createToolbox() {
+        // Build the toolbox
+        const toolboxCategories = [];
+        console.log({ blockCategories });
+        blockCategories.forEach((category) => {
+            const containsBlocks = flowEditorBlocks.current.filter((b) => b.manifest.category === category.id);
+            console.log({ category, containsBlocks });
+            const formattedName = formatMessage({
+                id: `${category.extensionId}.flows.blockCategories.${category.id}.title`,
+                defaultMessage: category.id,
+            });
+
+            toolboxCategories.push({
+                kind: 'category',
+                name: formattedName,
+                contents: containsBlocks.map((b) => b.getBlocklyToolboxDef()),
+            });
+        });
+
+        return {
+            kind: 'categoryToolbox',
+            contents: toolboxCategories,
+        };
+    }
+
+    useEffect(() => {
+        registerBlocks();
+        setToolbox(createToolbox());
+    }, []);
+
+    console.log({ toolbox });
+    if (!toolbox) return;
+
+    return (
+        <BlocklyWorkspace
+            className="aaa"
+            onInject={handleInject}
+            injectOptions={{
+                toolbox: toolbox,
+                renderer: 'thrasos',
+                sounds: false,
+                scrollbars: true,
+            }}
+        />
+    );
+});
+
+export default FlowEditor;
--- client/src/Flows/pages/FlowEdit.page.tsx
+++ client/src/Flows/pages/FlowEdit.page.tsx
@@ -1,14 +1,24 @@
 import { Page } from '@tjallingf/react-utils';
 import { useParams } from 'react-router-dom';
-import { lazy } from 'react';
-const FlowBlocklyEditor = lazy(() => import('../components/FlowBlocklyEditor'));
+import { lazy, Suspense } from 'react';
+import ErrorBoundary from '@/ErrorBoundary';
+import useQuery from '@/hooks/useQuery';
+const FlowEditor = lazy(() => import('../components/FlowEditor'));
 
 const FlowEdit: React.FunctionComponent = () => {
     const { id } = useParams();
 
+    const blocks = useQuery<any[]>('flows/editor/blocks');
+    const blockCategories = useQuery<any[]>('flows/editor/block-categories');
+    if (blocks.isLoading) return <span>Loading blocks...</span>;
+
+    if (blockCategories.isLoading) return <span>Loading block categories...</span>;
+
     return (
         <Page id="FlowEdit">
-            <FlowBlocklyEditor />
+            <Suspense fallback={<span>Loading...</span>}>
+                <FlowEditor flowId={id} blocks={blocks.result} blockCategories={blockCategories.result} />
+            </Suspense>
         </Page>
     );
 };
