b183d15b026aaeb4c8f2acf53eb19b0163166495
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 21:08:26 +0200

    feat: wrap each Device in an ErrorBoundary

--- client/src/Devices/pages/Devices.page.tsx
+++ client/src/Devices/pages/Devices.page.tsx
@@ -2,34 +2,37 @@ import { Page, Container } from '@tjallingf/react-utils';
 import Masonry from '@/Masonry';
 import useQuery from '@/hooks/useQuery';
 import Device, { IDeviceProps } from '@/Devices/components/Device/Device';
+import ErrorBoundary from '@/ErrorBoundary';
 
 const Devices: React.FunctionComponent = () => {
-  const { result: devices } = useQuery<IDeviceProps[]>('devices');
-  // let { data: rooms } = useQuery<IRoomProps[]>(['rooms']);
+    const { result: devices } = useQuery<IDeviceProps[]>('devices');
+    // let { data: rooms } = useQuery<IRoomProps[]>(['rooms']);
 
-  const renderDevices = () => {
-    // if(typeof rooms == 'undefined' || !activeRoom) return null;
+    const renderDevices = () => {
+        // if(typeof rooms == 'undefined' || !activeRoom) return null;
 
-    // const filteredDevices = devices!.filter(props =>
-    //     rooms[3].devices.includes(props.id));
+        // const filteredDevices = devices!.filter(props =>
+        //     rooms[3].devices.includes(props.id));
+
+        return (
+            <Masonry
+                breakpointCols={{ default: 6, 1200: 5, 992: 3, 768: 3, 576: 2, 575.98: 1 }}
+                className="flex-row align-items-start"
+            >
+                {devices?.map((props) => (
+                    <ErrorBoundary>
+                        <Device {...props} key={props.id} />
+                    </ErrorBoundary>
+                ))}
+            </Masonry>
+        );
+    };
 
     return (
-      <Masonry
-        breakpointCols={{ default: 6, 1200: 5, 992: 3, 768: 3, 576: 2, 575.98: 1 }}
-        className="flex-row align-items-start"
-      >
-        {devices?.map((props) => (
-          <Device {...props} key={props.id} />
-        ))}
-      </Masonry>
+        <Page id="devices">
+            <Container>{renderDevices()}</Container>
+        </Page>
     );
-  };
-
-  return (
-    <Page id="devices">
-      <Container>{renderDevices()}</Container>
-    </Page>
-  );
 };
 
 export default Devices;
