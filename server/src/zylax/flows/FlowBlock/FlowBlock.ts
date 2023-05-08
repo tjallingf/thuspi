224f682131f8ca0f39c25a7a7d9682b0dbe06c6c
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 22:04:30 +0200

    feat: add FlowBlockManifest type

--- server/src/zylax/flows/FlowBlock/FlowBlock.ts
+++ server/src/zylax/flows/FlowBlock/FlowBlock.ts
@@ -1,7 +1,45 @@
 import { ExtensionModule } from '../../extensions';
-import FlowBlockManifest from '../FlowBlockManifest';
-import * as constants from './constants';
-import { logger } from '@/zylax/lib';
+// import FlowBlockManifest from '../FlowBlockManifest';
+
+export type FlowBlockManifestParameterValidTypes = 'string' | 'number' | 'boolean' | 'color' | 'any';
+export type FlowBlockManifestParameterType =
+    | FlowBlockManifestParameterValidTypes
+    | FlowBlockManifestParameterValidTypes[];
+export type FlowBlockManifestParameterValue = string | number | boolean | null | undefined;
+
+export type FlowBlockManifestParameterOption = {
+    key?: string;
+    label?: string;
+    value: FlowBlockManifestParameterValue;
+};
+
+export interface FlowBlockManifestParameter {
+    blockly?: any;
+    type: FlowBlockManifestParameterType;
+    options?: FlowBlockManifestParameterOption[];
+    shadow?: {
+        type?: FlowBlockManifestParameterType;
+        value?: any;
+    };
+}
+
+export interface FlowBlockManifest {
+    icon?: string;
+    category: string;
+    connections?: {
+        top?: boolean;
+        bottom?: boolean;
+    };
+    parameters?: {
+        [key: string]: FlowBlockManifestParameter;
+    };
+    statements?: {
+        [key: string]: Object;
+    };
+    output?: {
+        type?: FlowBlockManifestParameterType;
+    };
+}
 
 export default class FlowBlock extends ExtensionModule {
     constructor() {
