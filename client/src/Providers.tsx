dd5614e845d29560e87807e9f7bc25d43ac08ef6
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 20:51:39 +0200

    refactor: don't use CDN for socket.io

--- client/src/Providers.tsx
+++ client/src/Providers.tsx
@@ -2,19 +2,22 @@ import AuthProvider from '@/providers/AuthProvider';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/utils/queryClient';
 import LanguageProvider from '@/providers/LanguageProvider';
+import SocketProvider from '@/providers/SocketProvider';
 
 export interface IProvidersProps {
-  children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 const Providers: React.FunctionComponent<IProvidersProps> = ({ children }) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <LanguageProvider>{children}</LanguageProvider>
-      </AuthProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <LanguageProvider>
+                    <SocketProvider>{children}</SocketProvider>
+                </LanguageProvider>
+            </AuthProvider>
+        </QueryClientProvider>
+    );
 };
 
 export default Providers;
