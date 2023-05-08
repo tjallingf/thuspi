36d7dc565a9938f707443823df7b3aaf4a0858e1
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-08 21:54:49 +0200

    feat: add routes for blocks and block categories

--- server/src/routes/api/flows.ts
+++ server/src/routes/api/flows.ts
@@ -1,19 +1,70 @@
-import api from '@/server/apiRoute';
+import apiRoute from '@/server/apiRoute';
+import { logger } from '@/zylax';
+import { ExtensionController, Extension } from '@/zylax/extensions';
+import { FlowBlock, FlowBlockCategory, FlowBlockCategoryManifest, FlowBlockManifest } from '@/zylax/flows';
 import Flow from '@/zylax/flows/Flow';
 import { Express } from 'express';
+import _ from 'lodash';
 
-export default (app: Express) => {
-    app.get(
+export default (server: Express) => {
+    server.get(
         '/api/flows',
-        api(Flow, async (api, req) => {
-            await api.withCollection(api.getCollection());
+        apiRoute(Flow, async (route, req) => {
+            await route.respondWithCollection(route.getCollection());
         }),
     );
 
-    app.get(
+    server.get(
         '/api/flows/:id',
-        api(Flow, async (api, req) => {
-            await api.withResource(api.getResource(req.params.id));
+        apiRoute(Flow, async (route, req) => {
+            await route.respondWithDocument(route.getDocument(req.params.id));
+        }),
+    );
+
+    server.get(
+        '/api/flows/editor/blocks',
+        apiRoute(null, async (route, req) => {
+            const flowBlocks = ExtensionController.findAllModulesOfType(FlowBlock);
+
+            const result: Array<{ type: string; manifest: FlowBlockManifest }> = [];
+
+            _.forOwn(flowBlocks, (moduleClass, moduleSlug) => {
+                try {
+                    result.push({
+                        type: moduleSlug,
+                        manifest: moduleClass.prototype.getManifest(),
+                    });
+                } catch (err: any) {
+                    logger.error(err);
+                    return true;
+                }
+            });
+
+            route.respondWith(result);
+        }),
+    );
+
+    server.get(
+        '/api/flows/editor/block-categories',
+        apiRoute(null, async (route, req) => {
+            const flowBlockCategories = ExtensionController.findAllModulesOfType(FlowBlockCategory);
+            const result: any[] = [];
+
+            _.forOwn(flowBlockCategories, (moduleClass, moduleSlug) => {
+                const parsedModuleSlug = Extension.parseModuleSlug(moduleSlug);
+                try {
+                    result.push({
+                        id: parsedModuleSlug.moduleName,
+                        manifest: moduleClass.prototype.getManifest(),
+                        extensionId: parsedModuleSlug.extensionId,
+                    });
+                } catch (err: any) {
+                    logger.error(err);
+                    return true;
+                }
+            });
+
+            route.respondWith(result);
         }),
     );
 };
