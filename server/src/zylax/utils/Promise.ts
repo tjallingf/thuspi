ab793df0ebcb8899af7d434a5a20256794d33afc
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-13 20:27:00 +0200

    fix: use generics for object type

--- server/src/zylax/utils/Promise.ts
+++ server/src/zylax/utils/Promise.ts
@@ -3,11 +3,11 @@ import * as _ from 'lodash';
 /**
  * Similar to Promise.all(), but with an object instead of an array.
  */
-async function PromiseAllObject(object: Object): Promise<Object> {
+async function PromiseAllObject<T>(object: T): Promise<T> {
     return _.zipObject(_.keys(object), await Promise.all(_.values(object)))
 }
 
-async function PromiseAllSettledObject(object: Object): Promise<Object> {
+async function PromiseAllSettledObject<T>(object: T): Promise<T> {
     return _.zipObject(_.keys(object), await Promise.allSettled(_.values(object)))
 }
 
