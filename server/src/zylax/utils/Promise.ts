a9034f24d4a921916a360f0b1748d1c53a1d145a
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-14 17:09:41 +0200

    fix: fix types

--- server/src/zylax/utils/Promise.ts
+++ server/src/zylax/utils/Promise.ts
@@ -3,11 +3,11 @@ import * as _ from 'lodash';
 /**
  * Similar to Promise.all(), but with an object instead of an array.
  */
-async function PromiseAllObject<T>(object: T): Promise<T> {
+async function PromiseAllObject<T>(object: T): Promise<any> {
     return _.zipObject(_.keys(object), await Promise.all(_.values(object)))
 }
 
-async function PromiseAllSettledObject<T>(object: T): Promise<T> {
+async function PromiseAllSettledObject<T>(object: T): Promise<any> {
     return _.zipObject(_.keys(object), await Promise.allSettled(_.values(object)))
 }
 
--- server/src/zylax/utils/object.ts
+++ server/src/zylax/utils/object.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 // Convert {a: {b: 'value'}} to {'a.b': 'value'}
-export function flattenKeys(obj: Object, seperator = '.', path = []) {
+export function flattenKeys(obj: Object, seperator = '.', path: string[] = []): Record<string, any> {
     return !_.isObject(obj)
         ? { [path.join(seperator)]: obj }
         : _.reduce(obj, (cum, next, key) => _.merge(cum, flattenKeys(next, seperator, [...path, key])), {});
